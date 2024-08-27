0711d61831c73bcacdc8f5e9fb3b0f1d29ff285f
O027-MIFTAHUL ULUM S.Pd <ulumiftahul06@gmail.com>
2024-08-27 22:04:05 +0700

    nuxt

---
+++ nuxt/main.vue
@@ -0,0 +1,10 @@
+import { BButton } from "bootstrap-vue-next"
+<template>
+    <div>
+        <BButton variant="primary" @click="show = !show">Click me</BButton>
+        <BModal v-model="show">Test</BModal>
+    </div>
+</template>
+<script setup lang="ts">
+const show = ref(false);
+</script>
--- nuxt/nuxt.config.js
+++ nuxt/nuxt.config.js
@@ -1,5 +1,14 @@
 // nuxt.config.js/ts
 export default defineNuxtConfig({
     modules: ["@bootstrap-vue-next/nuxt"],
-    css: ["bootstrap/dist/css/bootstrap.min.css"],
+    bootstrapVueNext: {
+        composables: true, // Will include all composables
+        // composables: {useBreadcrumb: true, useColorMode: true, all: false}, // Will include only useBreadcrumb & useColorMode
+        // composables: {useBreadcrumb: false, useColorMode: false, all: true} // Will include everything except useBreadcrumb & useColorMode
+        directives: { all: true }, // Will include all directives
+        css: true, // Will include the module's CSS. If set to false, you can add the CSS manually in the 'css' property below
+    },
+    css: [
+        // 'bootstrap/dist/css/bootstrap.min.css' // Not necessary if `css: true`
+    ],
 });
