26ca335794c2c8a5cd7c9283a2d2e118dc17ecfe
O027-MIFTAHUL ULUM S.Pd <ulumiftahul06@gmail.com>
2024-08-28 09:30:19 +0700

    components

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,34 +1,16 @@
 import { useState } from "react";
-import reactLogo from "./assets/img/react.svg";
-import viteLogo from "./assets/img/vite.svg";
-import bootstrapLogo from "./assets/img/bootstrap.png";
 import Button from "react-bootstrap/Button";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import NavBar from "./components/NavBar";
+import SlideShow from "./components/SlideShow";
 
 function App() {
     const [count, setCount] = useState(0);
     return (
         <>
-            <div>
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img
-                        src={reactLogo}
-                        className="logo react"
-                        alt="React logo"
-                    />
-                </a>
-                <a href="https://getbootstrap.com/" target="_blank">
-                    <img
-                        src={bootstrapLogo}
-                        className="logo bootstrap"
-                        alt="Bootstrap logo"
-                    />
-                </a>
-            </div>
+            <NavBar />
+            <SlideShow />
             <h1>Vite + React + Bootstrap</h1>
             <div className="card">
                 <p className="text-primary">count is {count}</p>
---
+++ react/src/components/CarouselImage.jsx
@@ -0,0 +1,9 @@
+export default function CarouselImage() {
+    return (
+        <>
+            <div className="carousel-item active">
+                <img src="..." className="d-block w-100" alt="..." />
+            </div>
+        </>
+    );
+}
